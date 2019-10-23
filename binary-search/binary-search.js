a1a85ac09d89fc3ab004a46deeadeaa495cc3e14
Camryn Perry <camrynpaigeperry@gmail.com>
2019-10-23 15:40:43 -0400

    completed binary search

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -2,7 +2,27 @@
 
 // Complete this algo
 const binarySearch = (array, target) => {
-	
+	if(array.length === 1){
+		return (array[0] === target);
+	}
+	let middleIdx = Math.ceil(array.length / 2);
+	let middle = array[middleIdx];
+	console.log('current middle: ', middle);
+
+	if(target === middle){
+		console.log("found target! ", middle);
+		return true;
+	}else if(target < middle){
+		const left = array.slice(0, middleIdx);
+		console.log("left array: ", left);
+		return binarySearch(left, target);
+	}else if(target > middle){
+		 const right = array.slice(middleIdx);
+		 console.log("right array: ", right);
+		 return binarySearch(right, target);
+	}else{
+		return false;
+	}
 };
 
 /*
